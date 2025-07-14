5d26cd9942fae49b4e3e7667363f6b37818c9941
hoan277 <chudinhhoan277@gmail.com>
2025-07-14 20:28:20 +0700

    clean sw

--- sw.js
+++ sw.js
@@ -1,41 +1,35 @@
-const CACHE_NAME = "v1_cache_panel_adm",
-  urlsToCache = [
-    "https://script.google.com/macros/s/AKfycbzHbfd-E6hvtEsYrPcvYYH4HQHGnTg099UUOsmGepZuoEwdIODBoigEkL-CdcW1fzTfTA/exec",
-    "./manifest.json",
-  ];
+const CACHE_NAME = "v1_cache_panel_adm";
+const urlsToCache = ["./manifest.json"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
-      })
-      .catch((err) => "Đăng ký bộ nhớ đệm không thành công: ", err)
+    caches.open(CACHE_NAME).then((cache) =>
+      Promise.all(
+        urlsToCache.map((url) =>
+          cache.add(url).catch((err) => {
+            console.warn("Không cache được:", url, err);
+          })
+        )
+      ).then(() => self.skipWaiting())
+    )
   );
 });
 
 self.addEventListener("activate", (e) => {
-  const caheWitheList = [CACHE_NAME];
-
+  const whitelist = [CACHE_NAME];
   e.waitUntil(
-    caches.keys().then((cachesNames) =>
-      cachesNames.map((cacheName) => {
-        if (cacheName.indexOf(cacheName) === -1) {
-          return caches.delete(cacheName);
-        }
-      })
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (!whitelist.includes(key)) return caches.delete(key);
+        })
+      )
     )
   );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      if (res) {
-        return res;
-      }
-      return fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
